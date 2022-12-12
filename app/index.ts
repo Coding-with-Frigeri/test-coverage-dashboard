eca576c6394824e343a2ba46536e873a8505ad13
Bruno Frigeri <bpiraja97@gmail.com>
2022-12-12 15:39:47 -0300

    feat: add first action

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,19 @@
-export const runTestCoverageDashboard = () => {
-  console.log('SOMETHING HAPPENING HERE')
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+const runTestCoverageDashboard = async () => {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet')
+    console.log(`Hello ${nameToGreet}!`)
+    const time = new Date().toTimeString()
+    core.setOutput('time', time)
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
+  } catch (err) {
+    core.setFailed(err.message)
+  }
 }
 
 runTestCoverageDashboard()
