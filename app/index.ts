025349e03df82bce3f194a288e1d7dec629c85fc
Bruno Frigeri <bpiraja97@gmail.com>
2022-12-13 19:19:35 -0300

    feat: testing

--- app/index.ts
+++ app/index.ts
@@ -1,23 +1,31 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
-// import * as fs from 'fs'
+// const github = require('@actions/github')
+import * as fs from 'fs'
 
 const runTestCoverageDashboard = async () => {
-  // const response = fs.readFileSync('data.json', 'utf-8')
-
-  // console.log(response)
   try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet')
-    console.log(`Hello ${nameToGreet}!`)
-    const time = new Date().toTimeString()
-    core.setOutput('time', time)
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.head_commit, undefined, 2)
-    console.log(`The event payload: ${payload}`)
+    fs.writeFile('test.json', '[]', (err) => {
+      if (err) throw err
+      console.log('SAVED!!!!')
+    })
   } catch (err) {
     core.setFailed(err.message)
   }
+  // const response = fs.readFileSync('data.json', 'utf-8')
+
+  // console.log(response)
+  // try {
+  //   // `who-to-greet` input defined in action metadata file
+  //   const nameToGreet = core.getInput('who-to-greet')
+  //   console.log(`Hello ${nameToGreet}!`)
+  //   const time = new Date().toTimeString()
+  //   core.setOutput('time', time)
+  //   // Get the JSON webhook payload for the event that triggered the workflow
+  //   const payload = JSON.stringify(github.context.head_commit, undefined, 2)
+  //   console.log(`The event payload: ${payload}`)
+  // } catch (err) {
+  //   core.setFailed(err.message)
+  // }
 }
 
 runTestCoverageDashboard()
