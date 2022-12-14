e32fbb1e0678105727c09fe17412279e7d460e0e
Bruno Frigeri <bpiraja97@gmail.com>
2022-12-14 10:13:26 -0300

    feat: try to run action.yml correctly

--- dist/app/functionsToTest.js
+++ dist/app/functionsToTest.js
@@ -1,23 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mod = exports.div = exports.mul = exports.sub = exports.sum = void 0;
 function sum(a, b) {
     return a + b;
 }
-exports.sum = sum;
 function sub(a, b) {
     return a - b;
 }
-exports.sub = sub;
 function mul(a, b) {
     return a * b;
 }
-exports.mul = mul;
 function div(a, b) {
     return a / b;
 }
-exports.div = div;
 function mod(a, b) {
     return a % b;
 }
-exports.mod = mod;
+export { sum, sub, mul, div, mod };
