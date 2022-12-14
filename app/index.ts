739313727afbb3d7398c984d09f05f52491afd8b
Bruno Frigeri <bpiraja97@gmail.com>
2022-12-14 14:27:28 -0300

    feat: test new script

--- app/index.ts
+++ app/index.ts
@@ -1,13 +1,33 @@
 import * as github from '@actions/github'
 import * as core from '@actions/core'
 import * as fs from 'fs'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 import { Data } from '../types/Data'
+import { execSync } from 'child_process'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const execCommandLine = (command: string) =>
+  execSync(command, { cwd: __dirname }).toString()
+
+const filesChanged = (hash: string) => {
+  const response = execCommandLine(
+    `git diff --name-only ${hash}^ ${hash}`
+  ).split('\n')
+
+  return response.filter(Boolean)
+}
+
+const isSpecFile = (filePath: string) => filePath.includes('.spec')
+
+const isAnySpecFilesTouched = (filesChanged: string[]) =>
+  filesChanged.some((value) => isSpecFile(value))
 
 const runTestCoverageDashboard = async () => {
   try {
-    const oldData = fs.readFileSync('data.json', 'utf-8')
-
     const {
       author,
       message,
@@ -15,17 +35,24 @@ const runTestCoverageDashboard = async () => {
       url,
       timestamp,
     } = github.context.payload.head_commit
-    const parsedOldData: Data[] = JSON.parse(oldData)
 
-    parsedOldData.unshift({
-      id: hash,
-      author,
-      message,
-      url,
-      timestamp,
-    })
+    const files = filesChanged(hash)
 
-    fs.writeFileSync('data.json', JSON.stringify(parsedOldData))
+    if (isAnySpecFilesTouched(files)) {
+      const oldData = fs.readFileSync('data.json', 'utf-8')
+
+      const parsedOldData: Data[] = JSON.parse(oldData)
+
+      parsedOldData.unshift({
+        id: hash,
+        author,
+        message,
+        url,
+        timestamp,
+      })
+
+      fs.writeFileSync('data.json', JSON.stringify(parsedOldData))
+    }
   } catch (err) {
     core.setFailed((err as { message: string }).message)
   }
--- dist/app/functionsToTest.d.ts
+++
@@ -1,6 +0,0 @@
-declare function sum(a: number, b: number): number;
-declare function sub(a: number, b: number): number;
-declare function mul(a: number, b: number): number;
-declare function div(a: number, b: number): number;
-declare function mod(a: number, b: number): number;
-export { sum, sub, mul, div, mod };
